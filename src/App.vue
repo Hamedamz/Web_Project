e032774b75907d1b4fb339115628cb4b3eb7fe00
Hamed <amzhamed@gmail.com>
2018-12-05 17:59:57 +0330

    created match card

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <full-header :post="mainNews"></full-header>
+    <full-header :post="mainNews" :match="liveMatch"></full-header>
     <latest-news></latest-news>
     <latest-games></latest-games>
   </div>
@@ -25,7 +25,15 @@ export default {
               title: 'Belgium retain pole as Sweden, Serbia soar',
               image: 'static/i1.jpg',
               category: 'premiere league'
-          }
+          },
+          liveMatch: {
+              homeName: 'LIV',
+              homeBadge: 'static/liv.png',
+              awayName: 'MAN',
+              awayBadge: 'static/man.png',
+              result: '3 : 1',
+              time: "68'",
+          },
       }
   }
 }
--- src/components/MatchCard.vue
+++ src/components/MatchCard.vue
@@ -1,15 +1,62 @@
 <template>
-    <a class="match-card">
-        Match Card
+    <a class="match-card  ui inverted">
+        <div class="team-badge">
+            <img :src="match.homeBadge" :alt="match.homeName">
+        </div>
+        <div class="match-info">
+            <div class="match-result">
+                <span>{{match.homeName}}</span>
+                <span class="result">{{match.result}}</span>
+                <span>{{match.awayName}}</span>
+            </div>
+            <div class="match-time">{{match.time}}</div>
+        </div>
+        <div class="team-badge">
+            <img :src="match.awayBadge" :alt="match.awayName">
+        </div>
     </a>
 </template>
 
 <script>
     export default {
         name: "MatchCard",
+        props: ['match'],
     }
 </script>
 
 <style scoped>
+    .match-card {
+        color: white;
+        max-width: 400px;
+        display: flex;
+        justify-content: space-between;
+    }
 
+    .match-info {
+        flex: auto;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+
+    }
+
+    .match-result {
+        display: flex;
+        justify-content: space-around;
+        font-weight: bold;
+    }
+
+    .result {
+        font-size: xx-large;
+    }
+
+    .match-time {
+        color: rgba(255, 255, 255, 0.5);
+        margin: 0.5rem 0;
+    }
+
+    .match-card img {
+        max-height: 70px;
+    }
 </style>
