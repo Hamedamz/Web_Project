82061f72aced00eee63db64389b44305855155e1
sfch1999 <sor100100@yahoo.com>
2018-12-06 13:10:01 +0330

    bootRemove

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import SuiVue from 'semantic-ui-vue'
 import 'semantic-ui-css/semantic.min.css'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue);
 
