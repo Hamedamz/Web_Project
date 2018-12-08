1a3465bf80ba29775536921a511cba32c75e7fbd
sfch1999 <sor100100@yahoo.com>
2018-12-08 23:14:59 +0330

    report

--- src/components/MatchReport.vue
+++ src/components/MatchReport.vue
@@ -1,13 +1,39 @@
 <template>
-    <a>Report</a>
+    <div>
+        <sui-table single-line stackable inverted striped selectable color="black">
+            <sui-table-body>
+                <match-event v-for="ne in news" :news="ne" :key="ne.id"></match-event>
+            </sui-table-body>
+        </sui-table>
+    </div>
 </template>
 
 <script>
+    import MatchEvent from "@/components/MatchEvent"
+
     export default {
-        name: "MatchReport"
+        name: "MatchReport",
+        components: {
+            MatchEvent
+        },
+        data(){
+            return {
+                news:[{
+                    time:69,
+                    main:'ahmad agha khata kard.',
+                    icon:'&#x003C;',
+                    id:0
+                },{
+
+                    time:9,
+                    main:'mamad agha corner gereft.',
+                    icon:'&#x0036;',
+                    id:1
+                }]
+            }
+        },
     }
 </script>
 
 <style scoped>
-
 </style>
