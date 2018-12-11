5e83f07f2a46e4629797fa18725292838fda60f6
Hamed <amzhamed@gmail.com>
2018-12-11 15:52:46 +0330

    added matches table and match cell

--- src/components/MatchCell.vue
+++ src/components/MatchCell.vue
@@ -1,15 +1,51 @@
 <template>
-    <div id="match-cell">
-
-    </div>
+    <a is="sui-grid-column" class="match-cell" :width="8" href="#">
+        <div class="teams"><div>
+            <sui-image :src="match.homeBadge" avatar></sui-image>
+            <span>{{match.homeName}}</span>
+            <span class="res">{{homePt}}</span>
+        </div>
+        <div>
+            <sui-image :src="match.awayBadge" avatar></sui-image>
+            <span>{{match.awayName}}</span>
+            <span class="res">{{awayPt}}</span>
+        </div></div>
+        <div class="time">{{match.time}}</div>
+    </a>
 </template>
 
 <script>
     export default {
-        name: "MatchCell"
+        name: "MatchCell",
+        props: ['match'],
+        computed: {
+            homePt: function () {
+                return this.match.result.split(":")[0];
+            },
+            awayPt: function () {
+                return this.match.result.split(":")[1];
+            },
+        }
     }
 </script>
 
 <style scoped>
-
+    .match-cell {
+        color: white;
+        display: flex !important;
+    }
+    .teams {
+        flex-basis: 70%;
+    }
+    .res {
+        float: right;
+        padding-right: 1rem;
+    }
+    .time {
+        height: 100%;
+        text-align: center;
+        border-left: solid 2px rgba(255, 255, 255, 0.1);
+        flex-grow: 1;
+        padding-top: 1rem;
+    }
 </style>
--- src/components/MatchTable.vue
+++ src/components/MatchTable.vue
@@ -1,14 +1,66 @@
 <template>
     <div id="match-table">
-        <sui-table>
+        <sui-grid class="container-fluid stackable">
+            <match-cell
+                v-for="match in matches"
+                :match="match"
+                ></match-cell>
 
-        </sui-table>
+        </sui-grid>
     </div>
 </template>
 
 <script>
+    import MatchCell from "@/components/MatchCell";
     export default {
-        name: "MatchTable"
+        name: "MatchTable",
+        components: {MatchCell},
+        data () {
+            return {
+                matches: [
+                    {
+                        homeName: 'LIV',
+                        homeBadge: 'static/liv.png',
+                        awayName: 'MAN',
+                        awayBadge: 'static/man.png',
+                        result: '3 : 1',
+                        time: "68'",
+                    },
+                    {
+                        homeName: 'ARS',
+                        homeBadge: 'static/ars.png',
+                        awayName: 'TOT',
+                        awayBadge: 'static/tot.png',
+                        result: '2 : 1',
+                        time: "45'",
+                    },
+                    {
+                        homeName: 'JUV',
+                        homeBadge: 'static/juv.png',
+                        awayName: 'MIL',
+                        awayBadge: 'static/mil.png',
+                        result: '0 : 0',
+                        time: "15'",
+                    },
+                    {
+                        homeName: 'PSG',
+                        homeBadge: 'static/psg.png',
+                        awayName: 'RSB',
+                        awayBadge: 'static/rsb.png',
+                        result: '11/12',
+                        time: "11:30 PM",
+                    },
+                    {
+                        homeName: 'BAR',
+                        homeBadge: 'static/bar.png',
+                        awayName: 'ATM',
+                        awayBadge: 'static/atm.png',
+                        result: '2 : 0',
+                        time: "FT",
+                    },
+                ],
+            }
+        }
     }
 </script>
 
