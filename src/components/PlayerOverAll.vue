db6f6d082913a0f44a1d7e53948575f06d5732d3
sfch1999 <sor100100@yahoo.com>
2018-12-11 23:26:36 +0330

    Team Complete

--- src/components/PlayerOverAll.vue
+++ src/components/PlayerOverAll.vue
@@ -1,13 +1,52 @@
 <template>
-
+    <sui-table-cell>
+        <div class="container">
+            <a :href="person.link">
+                <sui-item-group>
+                    <sui-item>
+                        <sui-item-image :src="person.image"/>
+                        <sui-item-content>
+                            <sui-item-header class="main">{{person.name}}</sui-item-header>
+                            <sui-item-meta>
+                                <span class="main red">{{person.post}}</span>
+                            </sui-item-meta>
+                            <sui-item-description>
+                                <p class="main">
+                                    Age:{{person.age}}
+                                </p>
+                            </sui-item-description>
+                            <sui-item-extra class="main">
+                                Joined in {{person.joinedYear}}
+                            </sui-item-extra>
+                        </sui-item-content>
+                    </sui-item>
+                </sui-item-group>
+            </a>
+        </div>
+    </sui-table-cell>
 </template>
 
 <script>
+
     export default {
-        name: "PlayerOverAll"
+        name: "PlayerOverAll",
+        components: {},
+        props: ['person'],
     }
 </script>
 
 <style scoped>
+    .main {
+        color: #d8d8d8 !important;
+    }
 
+    .red {
+        color: #d90412 !important;
+    }
+
+    .container {
+        background: rgba(0, 0, 0, 0.3);
+        padding: 10px;
+        width: 100%;
+    }
 </style>
--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -11,13 +11,30 @@
                     </sui-item>
                 </sui-item-group>
                 <div class="button">
-                    <sui-button circular color="red" @click="subscribe" :icon="icon" :basic="!subscribed"><span v-if="!subscribed">subscirbe</span></sui-button>
+                    <sui-button circular color="red" @click="subscribe" :icon="icon" :basic="!subscribed"><span
+                            v-if="!subscribed">subscirbe</span></sui-button>
                 </div>
             </div>
         </template>
         <template slot="main">
-            <div>
-                fj
+            <div class="league-menu">
+                <sui-button-group :widths="4" class="padded">
+                    <sui-button
+                            v-for="tab in tabs"
+                            :key="tab.name"
+                            @click="currentTab = tab"
+                            color="red"
+                            :basic="currentTab.name !== tab.name"
+                            :content="tab.name"
+                    ></sui-button>
+                </sui-button-group>
+                <keep-alive>
+                    <component
+                            v-bind:is="currentTabComponent"
+                            v-bind="currentTabProperties"
+                            class="tab"
+                    ></component>
+                </keep-alive>
             </div>
         </template>
     </full-page-image-container>
@@ -25,10 +42,13 @@
 
 <script>
     import FullPageImageContainer from "../layouts/FullPageImageContainer";
+    import TeamPlayers from "@/components/TeamPlayers"
+    import LatestNews from "@/components/LatestNews"
+    import MatchTable from "@/components/MatchTable"
 
     export default {
         name: "Team",
-        components: {FullPageImageContainer},
+        components: {FullPageImageContainer, TeamPlayers, LatestNews, MatchTable},
         data() {
             return {
                 team: {
@@ -36,19 +56,124 @@
                     camp: 'static/psg-stadium.jpg',
                     icon: 'static/psg.png'
                 },
-                subscribed:false
+                posts: [
+                    {
+                        id: 1,
+                        title: 'A home for the pride key to roaring Lions',
+                        image: 'http://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2018%2F1128%2Fr470200_1296x729_16%2D9.png&w=544&h=306&scale=crop&cquality=80&location=origin',
+                        type: 'small',
+                        category: 'premiere league',
+                        sport: 'football',
+                    },
+                    {
+                        id: 3,
+                        title: 'Wright Thompson: Italian football gets in your blood',
+                        image: 'http://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2018%2F0929%2Fr438829_1296x729_16%2D9.jpg&w=544&h=306&scale=crop&cquality=80&location=origin',
+                        type: 'small',
+                        category: 'premiere league',
+                        sport: 'football',
+                    },
+                    {
+                        id: 4,
+                        title: 'By the numbers: Ronaldo equals 51-year-old record',
+                        image: 'http://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2018%2F0816%2Fr415461_2_608x342_16%2D9.jpg&w=544&h=306&scale=crop&cquality=80&location=origin',
+                        type: 'small',
+                        category: 'serie A',
+                        sport: 'football',
+                    },
+                    {
+                        id: 5,
+                        title: 'Why Liverpool should sell Salah',
+                        image: 'http://a4.espncdn.com/combiner/i?img=%2Fphoto%2F2018%2F0816%2Fr415458_608x342_16%2D9.jpg&w=544&h=306&scale=crop&cquality=80&location=origin',
+                        type: 'small',
+                        category: 'premiere league',
+                        sport: 'football',
+                    },
+                    {
+                        id: 6,
+                        title: 'Who\'s more important to Man United: Pogba or Mourinho?',
+                        image: 'http://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2018%2F0816%2Fr415457_608x342_16%2D9.jpg&w=544&h=306&scale=crop&cquality=80&location=origin',
+                        type: 'big',
+                        category: 'premiere league',
+                        sport: 'football',
+                    },
+                    {
+                        id: 7,
+                        title: 'Kylian Mbappe and the curse of winning a World Cup',
+                        image: 'http://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2018%2F0925%2Fr436811_1296x729_16%2D9.jpg&w=544&h=306&scale=crop&cquality=80&location=origin',
+                        type: 'small',
+                        category: 'World Cup',
+                        sport: 'football',
+                    },
+                ],
+                subscribed: false,
+                tabs: [{name: 'News', comp: 'latest-news'}, {name: 'Squads', comp: 'team-players'}, {
+                    name: 'Matches',
+                    comp: 'match-table'
+                }],
+                currentTab: {name: 'News', comp: 'latest-news'},
+                people:
+                    {
+                        players: [{
+                            name: 'Michael Jordan',
+                            age: 27,
+                            joinedYear: 2013,
+                            post: 'HalfBack',
+                            image:'static/michael-avatar.png',
+                            link:'http://localhost:8080/player'
+                        },{
+                            name: 'Daniel Alba',
+                            age: 32,
+                            joinedYear: 2018,
+                            post: 'Left Defender',
+                            image:'static/michael-avatar.png',
+                            link:'http://localhost:8080/player'
+                        },{
+                            name: 'Dani',
+                            age: 22,
+                            joinedYear: 2010,
+                            post: 'Right Attacker',
+                            image:'static/michael-avatar.png',
+                            link:'http://localhost:8080/player'
+                        },],
+                        faculty:[{
+                            name: 'Carlos KeiRosh',
+                            age: 51,
+                            joinedYear: 2012,
+                            post: 'Sar Morrabi',
+                            image:'static/michael-avatar.png',
+                            link:'/#'
+                        },{
+                            name: 'Ahmad Peirovani',
+                            age: 65,
+                            joinedYear: 1960,
+                            post: 'komak Morrabi',
+                            image:'static/michael-avatar.png',
+                            link:'/#'
+                        }]
+                    }
             }
         },
-        methods:{
-            subscribe:function () {
+        methods: {
+            subscribe: function () {
                 this.subscribed = !this.subscribed
             }
         },
         computed: {
             icon: function () {
-                return this.subscribed ? 'like': '';
-            }
-        }
+                return this.subscribed ? 'like' : '';
+            },
+            currentTabComponent: function () {
+                return this.currentTab.comp
+            },
+            currentTabProperties: function () {
+                if (this.currentTabComponent === 'latest-news')
+                    return {posts: this.posts};
+                if (this.currentTabComponent === 'team-players')
+                    return {people: this.people};
+                return {};
+            },
+        },
     }
 </script>
 
@@ -62,9 +187,14 @@
         display: flex;
         justify-content: space-between;
     }
-    .button{
+
+    .button {
         display: flex;
         flex-direction: column;
         justify-content: flex-end;
+    }
+
+    .padded {
+        padding-bottom: 1rem;
     }
 </style>
