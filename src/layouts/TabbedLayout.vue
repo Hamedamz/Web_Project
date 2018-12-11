2cb46b904dd2c5cf2f739e0713bc6beb73668811
Hamed <amzhamed@gmail.com>
2018-12-11 10:44:58 +0330

    added some assets

---
+++ src/layouts/TabbedLayout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="tabbed-menu">
+        <sui-button-group :widths="5">
+            <sui-button
+                    v-for="tab in tabs"
+                    :key="tab"
+                    @click="currentTab = tab"
+                    color="red"
+                    :basic="currentTab !== tab"
+                    :content="tab"
+            ></sui-button>
+        </sui-button-group>
+        <keep-alive>
+            <component
+                    v-bind:is="currentTabComponent"
+                    v-bind="currentTabProperties"
+                    class="tab"
+            ></component>
+        </keep-alive>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TabbedLayout"
+    }
+</script>
+
+<style scoped>
+
+</style>
