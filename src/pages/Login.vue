f30c4cec90e6022cbcf21347a82a9d9dc4f446a2
Hamed <amzhamed@gmail.com>
2019-02-01 12:39:32 +0330

    forgot form created

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -10,6 +10,8 @@
                 <sui-form-field v-if="mode === 'login'">
                     <label>Username</label>
                     <input type="text" name="username" v-model="input.username" placeholder="Username"/>
+                </sui-form-field>
+                <sui-form-field v-if="mode === 'login'">
                     <label>Password</label>
                     <input type="password" name="password" v-model="input.password" placeholder="Password"/>
                 </sui-form-field>
@@ -19,6 +21,26 @@
                 </sui-form-field>
                 <sui-button secondary type="button" v-on:click.prevent="login()" v-if="mode === 'login'">Login</sui-button>
                 <sui-button secondary type="button" v-on:click.prevent="forgot()" v-if="mode === 'forgot'">Send</sui-button>
+            </sui-form>
+
+            <sui-form v-if="mode === 'forgot'">
+                <sui-form-field>
+                    <label>uid</label>
+                    <input type="text" name="uid" v-model="reset.uid" placeholder="Username"/>
+                </sui-form-field>
+                <sui-form-field>
+                    <label>token</label>
+                    <input type="password" name="token" v-model="reset.token" placeholder="Password"/>
+                </sui-form-field>
+                <sui-form-field>
+                    <label>new password</label>
+                    <input type="password" name="new_password1" v-model="reset.new_password1" placeholder="Password"/>
+                </sui-form-field>
+                <sui-form-field>
+                    <label>new password confirm</label>
+                    <input type="password" name="new_password2" v-model="reset.new_password2" placeholder="Password Confirm"/>
+                </sui-form-field>
+                <sui-button positive type="button" v-on:click.prevent="resetPass()">Reset</sui-button>
             </sui-form>
 
         </div>
@@ -40,6 +62,12 @@
                 },
                 email:{
                     email:"",
+                },
+                reset: {
+                    uid: '',
+                    token: '',
+                    new_password1: '',
+                    new_password2: '',
                 }
             }
         },
@@ -55,6 +83,11 @@
             },
             forgot() {
                 this.$http.post(APIService.AUTH+'password/reset/', this.email, {emulateJSON: true})
+                    .then(response => response.json())
+                    .catch(error => console.log(error))
+            },
+            resetPass() {
+                this.$http.post(APIService.AUTH+'password/reset/confirm/', this.reset, {emulateJSON: true})
                     .then(response => response.json())
                     .catch(error => console.log(error))
             },
